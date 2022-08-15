3beeac0648f60d0677cc1dfe0428304149e8a9e5
kdy4539 <kdy4539@naver.com>
2022-08-16 01:58:38 +0900

    Update slideShow.js

--- JS/slideShow.js
+++ JS/slideShow.js
@@ -11,7 +11,7 @@ const slideMargin = 100; //슬라이드간의 margin 값
 slides.style.width = (slideWidth + slideMargin) * slideCount + 'px';
 
 function moveSlide(num) {
-  slides.style.left = -num * 1380 + 'px';
+  slides.style.left = -num * (slideWidth + slideMargin) + 'px';
   currentIdx = num;
 }
 
